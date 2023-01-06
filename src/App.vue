de34cc67e5ff6a2f251a5b21e5f4a3f4bff758f0
Marian Minar <majamin@gmail.com>
2023-01-06 15:03:05 -0800

    add simpler styling and content

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <script setup lang="ts"></script>
 
 <template>
-  <main class="flex flex-col space-y-8 w-fit mx-auto my-12">
+  <main class="mx-auto my-12 flex w-fit flex-col space-y-8">
     <div>
       <div class="text-2xl font-bold">Vite + Vue + Tailwind Starter</div>
       <div>A quick template for rapid development.</div>
     </div>
-    <div></div>
+    <div class="text-gray-500">
+      <ul>
+        <li>Tailwind (with normal defaults)</li>
+        <li>aliased @ to refer to src directory</li>
+      </ul>
+    </div>
   </main>
 </template>
